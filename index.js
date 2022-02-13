e58736f7fb6d49d4cb2c2a795c85df594a4775d3
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-12 23:26:52 -0500

    commit tree 9a63e849811c041b3ec45cde5589c6ff3b9ab527

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let objectNums = {}
+  for (let i = 0; i < array.length; i++) {
+    if (objectNums[array[i]]) return true
+    objectNums[ target - array[i] ] = true
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+  loop through array.  
+  find complement of array number.
+  if number is in object, return true
+  else, put complement in object
+  after loop, return false
 */
 
 /*
@@ -17,6 +29,9 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 7], 10));
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
